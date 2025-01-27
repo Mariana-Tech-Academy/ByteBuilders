95e3163b2e9edce01e829f94bedff9730e63ef2f
Oreva Eniworo <eniworoeva@yahoo.com>
2025-01-27 17:17:42 +0100

    refactor: reorganize routes for user and admin controllers, streamline borrowing functionality

--- routes/route.go
+++ routes/route.go
@@ -7,30 +7,23 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine,
-	bookController *controllers.BookController,
-	borrowController *controllers.BorrowController,
 	adminController *controllers.AdminController,
 	userController *controllers.UserController) {
 
 	// Public authentication routes
 	r.POST("/signup", userController.Register)
 	r.POST("/login", userController.Login)
-	r.POST("/logout", userController.Logout)
-	r.GET("/getauthors", userController.GetAuthors)
-	// Book routes
-	r.DELETE("/deletebook/:id", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), bookController.DeleteBook)
 
-	// Borrow and return routes
-
-	r.POST("/borrowbook/:id", middleware.AuthMiddleware(), borrowController.BorrowBook)
+	// User routes
+	r.GET("/user", middleware.AuthMiddleware(), userController.GetUserByUsername)
+	r.POST("/borrowbook/:id", middleware.AuthMiddleware(), userController.BorrowBook)
+	r.POST("/logout", middleware.AuthMiddleware(), userController.Logout)
+	r.GET("/getauthors", middleware.AuthMiddleware(), userController.GetAuthors)
 
 	// Admin routes for user management
-	r.POST("/addbook",middleware.AuthMiddleware(),adminController.AddBook)
-
+	r.POST("/addbook", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), adminController.AddBook)
 	r.PUT("/updatebook", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), adminController.UpdateBook)
-
-	r.GET("/user", middleware.AuthMiddleware(), userController.GetUserByUsername)
-
+	r.DELETE("/deletebook/:id", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), adminController.DeleteBook)
 	r.POST("/addauthor", middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"), adminController.AddAuthor)
 
 }
